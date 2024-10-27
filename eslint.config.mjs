a5fd2b03e647f4bfa8308ff423396ab4ff2f31c6
jobet1995 <jobetcasquejo221@gmail.com>
2024-10-27 13:48:22 +0800

    added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,17 +15,17 @@ export default [
     rules: {
       // JSDoc rules
       "jsdoc/require-jsdoc": ["error"], // Require JSDoc comments
-      "jsdoc/require-param": ["warn"], // Warn for missing JSDoc param comments
-      "jsdoc/require-returns": ["warn"], // Warn for missing JSDoc return comments
+      "jsdoc/require-param": ["warn"],   // Warn for missing JSDoc param comments
+      "jsdoc/require-returns": ["warn"],  // Warn for missing JSDoc return comments
     },
   },
   {
     // Override settings for Page.js and Layout.js
     files: ["**/Page.js", "**/Layout.js"],
     rules: {
-      "jsdoc/require-jsdoc": "off", // Disable JSDoc requirement
-      "jsdoc/require-param": "off", // Disable missing param warnings
-      "jsdoc/require-returns": "off", // Disable missing return warnings
+      "jsdoc/require-jsdoc": "off",    // Disable JSDoc requirement
+      "jsdoc/require-param": "off",     // Disable missing param warnings
+      "jsdoc/require-returns": "off",   // Disable missing return warnings
     },
   },
   pluginJs.configs.recommended,
